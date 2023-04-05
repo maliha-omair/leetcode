469f2516d0c06b31ae3ad4c4a26bf37c92334afa
Maliha Omair <maliha@malihaomair.com>
2023-04-04 20:59:11 -0400

    Time: 62 ms (16.78%), Space: 14.5 MB (32.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,33 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        dict = {}
         if len(t) != len(s):
             return False
         for indx in range (0,len(s)):
-            t = t.replace(s[indx],'',1)
-            # key = s[indx]
-            # if key in dict.keys():
-            #     dict[key] = dict[key]+1
-            # else:
-                # dict[key] = 0 
-        print("t is", t)
-        if t: 
-            return False
-        else: 
-            return True
-        # print ("dict is", dict)
+            key = s[indx]
+            if key in dict.keys():
+                dict[key] = dict[key]+1
+            else:
+                dict[key] = 1 
         
+        print ("dict is", dict)
+        count = len(dict)
+        for indx in range (0,len(t)):
+            
+            key = t[indx]
+            if key in dict.keys():
+                dict[key] = dict[key]-1
+                if dict[key] <= -1:
+                    return False
+                count = count - 1 
+            else:
+                return False
+            
+        print("values in dict", dict, " count", count)
+                
+        if count <= 0:
+            return True
+        else:
+            return False
+                                  
+     
